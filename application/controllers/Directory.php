282ff7350d396641a6980d9f1e01a0469179903e
JackLambda <sebastien.chillet@gmail.com>
2017-05-29 16:36:02 +0200

    some modifications

--- application/controllers/Directory.php
+++ application/controllers/Directory.php
@@ -7,10 +7,18 @@
  */
 defined('BASEPATH') OR exit ('No direct script access allowed');
 
-Class Directory extends CI_Controller {
+Class Directory extends CI_Controller  {
+
+    public function __construct(){
+
+        parent::__construct();
+        $this->load->helper('url_helper');
+
+    }
 
     public function index () {
 
+        $config['first_url'] = site_url();
         $this->load->view('v_directory');
 
     }
