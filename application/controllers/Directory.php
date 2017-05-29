b315aa8b6e02d5b887a6ddde0b94833e0fff7d62
JackLambda <sebastien.chillet@gmail.com>
2017-05-29 16:18:10 +0200

    create class and public function index()

--- application/controllers/Directory.php
+++ application/controllers/Directory.php
@@ -5,5 +5,16 @@
  * Date: 29/05/2017
  * Time: 16:07
  */
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+Class Directory extends CI_Controller {
+
+    public function index () {
+
+        $this->load->view('v_directory');
+
+    }
+
+}
 
 ?>
